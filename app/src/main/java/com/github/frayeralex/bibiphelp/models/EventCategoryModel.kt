6f5a3b57238ddff4b5da828c137b09afc0e874e5
Alex Boyko <frayeralex@gmail.com>
2020-02-23 14:06:12 +0200

    #15: fix data models, add Utility classes

--- app/src/main/java/com/github/frayeralex/bibiphelp/models/EventCategoryModel.kt
+++ app/src/main/java/com/github/frayeralex/bibiphelp/models/EventCategoryModel.kt
@@ -1,28 +1,23 @@
 package com.github.frayeralex.bibiphelp.models
 
 import com.github.frayeralex.bibiphelp.R
+import com.github.frayeralex.bibiphelp.constatns.EventTypes
 
 
-data class EventCategoryModel (
-    var id: Int? = OTHER,
+data class EventCategoryModel(
+    var id: Int? = EventTypes.OTHER,
     var label: String? = ""
-) {
-    fun getImgResource(): Int = when (id) {
-        OIL -> R.drawable.category_1_icon
-        WHEEL -> R.drawable.category_2_icon
-        ENERGY -> R.drawable.category_3_icon
-        SNOW -> R.drawable.category_4_icon
-        TOWING -> R.drawable.category_5_icon
-        OTHER -> R.drawable.category_6_icon
-        else -> R.drawable.category_6_icon
-    }
+)
 
-    companion object {
-        const val OIL = 1
-        const val WHEEL = 2
-        const val ENERGY = 3
-        const val SNOW = 4
-        const val TOWING = 5
-        const val OTHER = 6
+object EventCategoryModelUtils {
+    @JvmStatic
+    fun getImgResource(eventCategory: EventCategoryModel): Int = when (eventCategory.id) {
+        EventTypes.OIL -> R.drawable.category_1_icon
+        EventTypes.WHEEL -> R.drawable.category_2_icon
+        EventTypes.ENERGY -> R.drawable.category_3_icon
+        EventTypes.SNOW -> R.drawable.category_4_icon
+        EventTypes.TOWING -> R.drawable.category_5_icon
+        EventTypes.OTHER -> R.drawable.category_6_icon
+        else -> R.drawable.category_6_icon
     }
 }
--- app/src/main/java/com/github/frayeralex/bibiphelp/models/EventModel.kt
+++ app/src/main/java/com/github/frayeralex/bibiphelp/models/EventModel.kt
@@ -1,42 +1,42 @@
 package com.github.frayeralex.bibiphelp.models
 
 import com.github.frayeralex.bibiphelp.R
+import com.github.frayeralex.bibiphelp.constatns.EventTypes
 import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.database.IgnoreExtraProperties
+
 //todo update - not final model schema
 @IgnoreExtraProperties
 data class EventModel(
     var id: String? = "",
-    var type: String? = "",
+    var type: Int? = EventTypes.OTHER,
     var message: String? = "",
+    var userId: String? = "",
     var lat: Double? = 0.0,
     var long: Double? = 0.0
-) {
-    private fun getPin(): BitmapDescriptor = when (type) {
-        TYPE_1 -> BitmapDescriptorFactory.fromResource(R.drawable.pin_1)
-        TYPE_2 -> BitmapDescriptorFactory.fromResource(R.drawable.pin_2)
-        TYPE_3 -> BitmapDescriptorFactory.fromResource(R.drawable.pin_3)
-        TYPE_4 -> BitmapDescriptorFactory.fromResource(R.drawable.pin_4)
-        TYPE_5 -> BitmapDescriptorFactory.fromResource(R.drawable.pin_5)
+)
+
+object EventModelUtils {
+    @JvmStatic
+    fun getPin(event: EventModel): BitmapDescriptor = when (event.type) {
+        EventTypes.OIL -> BitmapDescriptorFactory.fromResource(R.drawable.pin_1)
+        EventTypes.WHEEL -> BitmapDescriptorFactory.fromResource(R.drawable.pin_2)
+        EventTypes.ENERGY -> BitmapDescriptorFactory.fromResource(R.drawable.pin_3)
+        EventTypes.SNOW -> BitmapDescriptorFactory.fromResource(R.drawable.pin_4)
+        EventTypes.TOWING -> BitmapDescriptorFactory.fromResource(R.drawable.pin_5)
         else -> BitmapDescriptorFactory.fromResource(R.drawable.pin_5)
     }
 
-    private fun getCoordinates(): LatLng = LatLng(lat!!, long!!)
+    @JvmStatic
+    fun getCoordinates(event: EventModel): LatLng = LatLng(event.lat!!, event.long!!)
 
-    fun getMapMarker(): MarkerOptions = MarkerOptions()
-        .position(getCoordinates())
-        .title(message)
+    @JvmStatic
+    fun getMapMarker(event: EventModel): MarkerOptions = MarkerOptions()
+        .position(getCoordinates(event))
+        .title(event.message)
         .anchor((0.5).toFloat(), (0.5).toFloat())
-        .icon(getPin())
-
-    companion object {
-        const val TYPE_1 = "type_1"
-        const val TYPE_2 = "type_2"
-        const val TYPE_3 = "type_3"
-        const val TYPE_4 = "type_4"
-        const val TYPE_5 = "type_5"
-    }
+        .icon(getPin(event))
 }
