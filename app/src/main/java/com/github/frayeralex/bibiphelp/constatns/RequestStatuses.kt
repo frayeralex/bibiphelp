94977b70ade9a30a43b8b6442e00571b6971ae44
Alex Boyko <frayeralex@gmail.com>
2020-02-26 17:23:39 +0200

    #18: refactor constants

---
+++ app/src/main/java/com/github/frayeralex/bibiphelp/constatns/RequestStatuses.kt
@@ -0,0 +1,8 @@
+package com.github.frayeralex.bibiphelp.constatns
+
+object RequestStatuses {
+    const val UNCALLED = "UNCALLED"
+    const val PENDING = "PENDING"
+    const val SUCCESS = "SUCCESS"
+    const val FAILURE = "FAILURE"
+}
