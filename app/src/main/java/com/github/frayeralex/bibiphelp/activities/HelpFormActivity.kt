49d4a25a9f7b73efd6c1bdb31ff60f414849d6d5
Alex Boyko <frayeralex@gmail.com>
2020-02-28 16:25:44 +0200

    #15: improve HelpFormActivity styles

--- app/src/main/java/com/github/frayeralex/bibiphelp/activities/HelpFormActivity.kt
+++ app/src/main/java/com/github/frayeralex/bibiphelp/activities/HelpFormActivity.kt
@@ -2,16 +2,21 @@ package com.github.frayeralex.bibiphelp.activities
 
 import com.github.frayeralex.bibiphelp.R
 import android.content.Intent
+import android.content.res.ColorStateList
 import android.location.Location
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ProgressBar
-import android.widget.Toast
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.MenuItem
+import android.widget.*
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
+import com.github.frayeralex.bibiphelp.constatns.EventTypes
+import com.github.frayeralex.bibiphelp.constatns.InputValidation
 import com.github.frayeralex.bibiphelp.viewModels.HelpFormViewModel
 
 
@@ -22,8 +27,10 @@ class HelpFormActivity : AppCompatActivity() {
     private var categoryLabel = ""
 
     private var isRequesting = false
-    private var location : Location? = null
+    private var location: Location? = null
 
+    private lateinit var validationText: TextView
+    private lateinit var textCount: TextView
     private lateinit var progressBar: ProgressBar
     private lateinit var button: Button
     private lateinit var messageInput: EditText
@@ -31,8 +38,8 @@ class HelpFormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        categoryId = intent.getIntExtra(CategoriesActivity.CATEGORY_ID_KEY, 6)
-        categoryLabel = intent.getStringExtra(CategoriesActivity.CATEGORY_LABEL)
+        categoryId = intent.getIntExtra(CategoriesActivity.CATEGORY_ID_KEY, EventTypes.OTHER)
+        categoryLabel = intent.getStringExtra(CategoriesActivity.CATEGORY_LABEL)!!
 
         setContentView(R.layout.activity_help_form)
 
@@ -42,46 +49,113 @@ class HelpFormActivity : AppCompatActivity() {
 
         progressBar = findViewById(R.id.progressBar)
 
+        validationText = findViewById(R.id.validationText)
+
+        textCount = findViewById(R.id.textCount)
+        textCount.text = "0 / ${InputValidation.helpMsgMaxLength}"
+
         button = findViewById(R.id.helpRequestButton)
-        button.setOnClickListener{ handlePublishBtnClick() }
+        button.setOnClickListener { handlePublishBtnClick() }
 
         messageInput = findViewById(R.id.message)
 
-        viewModel.getLocationData().observe(this, Observer<Location> { location = it })
+        messageInput.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(cs: Editable) {}
 
-        viewModel.getRequestingState().observe(this, Observer<Boolean> {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(cs: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                viewModel.setMessage(cs.toString())
+            }
+        })
+
+        viewModel.getLocationData().observe(this, Observer<Location> { location = it })
+        viewModel.getValidationError().observe(this, Observer {
+            if (it != null) {
+                validationText.text = resources.getText(it)
+                validationText.isVisible = true
+            } else {
+                validationText.isVisible = false
+            }
+
+        })
+
+        viewModel.getMessage().observe(this, Observer<String> {
+            textCount.text = "${it.length} / ${InputValidation.helpMsgMaxLength}"
+            updateTextInputStyles(it.length <= InputValidation.helpMsgMaxLength)
+        })
+
+        viewModel.getRequestingState().observe(this, Observer {
             progressBar.isVisible = it
             isRequesting = it
         })
 
-        viewModel.getCreatedSuccess().observe(this, Observer<Boolean> {
-            if (it) {
-                val intent = Intent(this, MainActivity::class.java)
+        viewModel.getCreatedEventId().observe(this, Observer {
+            if (it != null) {
+                Log.d("OPACHA", it)
+                val intent = Intent(this, WaitHelpActivity::class.java)
+                intent.putExtra(EVENT_ID, it)
                 startActivity(intent)
-                finish()
             }
         })
     }
 
-    fun handlePublishBtnClick() {
+    private fun updateTextInputStyles(isValid: Boolean) {
+        if (isValid) {
+            messageInput.backgroundTintList =
+                ColorStateList.valueOf(ContextCompat.getColor(this, R.color.colorPrimary))
+            messageInput.setTextColor(ContextCompat.getColor(this, R.color.colorPrimary))
+            textCount.setTextColor(ContextCompat.getColor(this, R.color.colorPrimary))
+        } else {
+            messageInput.backgroundTintList =
+                ColorStateList.valueOf(ContextCompat.getColor(this, R.color.colorAccent))
+            messageInput.setTextColor(ContextCompat.getColor(this, R.color.colorAccent))
+            textCount.setTextColor(ContextCompat.getColor(this, R.color.colorAccent))
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        android.R.id.home -> {
+            super.onBackPressed()
+            true
+        }
+
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun handlePublishBtnClick() {
         if (isRequesting) return
 
         val message = messageInput.text.toString().trim()
 
-        if (message == "") {
+        if (message.isEmpty()) {
             return Toast.makeText(
                 baseContext, R.string.help_input_label_empty,
-                Toast.LENGTH_SHORT
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+        if (message.length > InputValidation.helpMsgMaxLength) {
+            return Toast.makeText(
+                baseContext, R.string.help_input_large_text,
+                Toast.LENGTH_LONG
             ).show()
         }
 
         if (location == null) {
+            //todo implement permission request
             return Toast.makeText(
                 baseContext, "Need location permissions",
-                Toast.LENGTH_SHORT
+                Toast.LENGTH_LONG
             ).show()
         }
 
         viewModel.createHelpRequest(message, categoryId!!, location!!)
+    }
+
+    companion object {
+        const val EVENT_ID = "EVENT_ID"
     }
 }
--- app/src/main/java/com/github/frayeralex/bibiphelp/viewModels/HelpFormViewModel.kt
+++ app/src/main/java/com/github/frayeralex/bibiphelp/viewModels/HelpFormViewModel.kt
@@ -2,51 +2,45 @@ package com.github.frayeralex.bibiphelp.viewModels
 
 import android.app.Application
 import android.location.Location
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import com.github.frayeralex.bibiphelp.R
+import com.github.frayeralex.bibiphelp.constatns.InputValidation
 import com.github.frayeralex.bibiphelp.liveDatas.LocationLiveData
-import com.github.frayeralex.bibiphelp.liveDatas.UserLiveData
-import com.github.frayeralex.bibiphelp.models.EventCategoryModel
 import com.github.frayeralex.bibiphelp.models.EventModel
 import com.github.frayeralex.bibiphelp.repository.FBRefs
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
 
 class HelpFormViewModel(application: Application) : AndroidViewModel(application) {
-    private val category : MutableLiveData<EventCategoryModel> = MutableLiveData()
+
+    private val validationErrorCode: MutableLiveData<Int?> = MutableLiveData(null)
+    private val message: MutableLiveData<String> = MutableLiveData("")
 
     private val location = LocationLiveData(application)
 
     private val isRequesting = MutableLiveData(false)
-    private val isCreatedSuccess = MutableLiveData(false)
-
-    fun getCategory(categoryId: String): LiveData<EventCategoryModel> {
-        if (category.value === null) {
-
-
-            FBRefs.categoriesRef.child(categoryId).addValueEventListener(object : ValueEventListener {
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    category.value = dataSnapshot.getValue(EventCategoryModel::class.java)
-                }
-
-                override fun onCancelled(error: DatabaseError) {  }
-            })
-        }
-        return category
-    }
+    private val newCreatedEventId = MutableLiveData<String?>(null)
 
     fun getLocationData() = location
     fun getRequestingState() = isRequesting
-    fun getCreatedSuccess() = isCreatedSuccess
+    fun getCreatedEventId() = newCreatedEventId
+    fun getMessage() = message
+    fun getValidationError() = validationErrorCode
+
+    fun setMessage(value: String) {
+        message.value = value
+
+        validationErrorCode.value = when {
+            value.length > InputValidation.helpMsgMaxLength -> R.string.help_input_large_text
+            value.isEmpty() -> R.string.help_input_label_empty
+            else -> null
+        }
+    }
 
     fun createHelpRequest(message: String, categoryId: Int, location: Location) {
         isRequesting.value = true
 
         val eventRef = FBRefs.eventsRef.push()
+        val eventId = eventRef.key
 
         val action = eventRef.setValue(EventModel(
             id = eventRef.key,
@@ -58,12 +52,10 @@ class HelpFormViewModel(application: Application) : AndroidViewModel(application
 
         action.addOnSuccessListener {
             isRequesting.value = false
-            isCreatedSuccess.value = true
+            newCreatedEventId.value = eventId
         }
         action.addOnCanceledListener { isRequesting.value = false }
     }
 
-    companion object {
-        const val TAG = "HelpFormViewModel"
-    }
+
 }
