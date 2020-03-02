fc3512658f7447792da14bca7b60c6985cd82612
Alex Boyko <frayeralex@gmail.com>
2020-03-02 14:25:02 +0200

    #27: add WaiteHelpViewModel

---
+++ app/src/main/java/com/github/frayeralex/bibiphelp/viewModels/WaitHelpViewModel.kt
@@ -0,0 +1,30 @@
+package com.github.frayeralex.bibiphelp.viewModels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.github.frayeralex.bibiphelp.models.*
+import com.github.frayeralex.bibiphelp.repository.FBRefs
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+
+class WaitHelpViewModel : ViewModel() {
+    private val event : MutableLiveData<EventModel> = MutableLiveData()
+
+    fun getEvent(eventId: String) : LiveData<EventModel> {
+        if (event.value == null) {
+            FBRefs.eventsRef.child(eventId).addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                    if (dataSnapshot.exists()) {
+                        event.value = dataSnapshot.getValue(EventModel::class.java)
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {}
+            })
+        }
+        return event
+    }
+}
